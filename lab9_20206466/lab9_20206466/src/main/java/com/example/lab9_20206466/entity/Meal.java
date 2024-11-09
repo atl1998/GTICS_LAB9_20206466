5f3e23d8cf6e4480b9317c1a3d318c127c365376
atl1998 <adrianbala0410@gmail.com>
2024-11-09 16:56:44 -0500

    lista de platillos y detalles por el nombre
    ok

---
+++ lab9_20206466/lab9_20206466/src/main/java/com/example/lab9_20206466/entity/Meal.java
@@ -0,0 +1,43 @@
+package com.example.lab9_20206466.entity;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class Meal {
+    private Integer idMeal;
+
+    private String strMeal;
+    private String strCategory;
+    private String strArea;
+    private String strInstructions;
+
+    private String strMealThumb;
+    private String strYoutube;
+    private String strSource;
+
+    private String strTags;
+
+    private String strIngredient1;
+    private String strIngredient2;
+    private String strIngredient3;
+    private String strIngredient4;
+    private String strIngredient5;
+    private String strIngredient6;
+    private String strIngredient7;
+    private String strIngredient8;
+    private String strIngredient9;
+    private String strIngredient10;
+    private String strIngredient11;
+
+    private String strMeasure1;
+    private String strMeasure2;
+    private String strMeasure3;
+    private String strMeasure4;
+    private String strMeasure5;
+    private String strMeasure6;
+    private String strMeasure7;
+    private String strMeasure8;
+    private String strMeasure9;
+    private String strMeasure10;
+    private String strMeasure11;
+}
---
+++ lab9_20206466/lab9_20206466/src/main/java/com/example/lab9_20206466/entity/MealResponse.java
@@ -0,0 +1,15 @@
+package com.example.lab9_20206466.entity;
+
+import java.util.List;
+
+public class MealResponse {
+    private List<Meal> meals;
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+}
